120c8e6db6556fe70d79980dedfade01993beeee
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-26 13:22:58 +0200

    Added documentation to class SchwimmenCard

--- src/main/kotlin/entity/SchwimmenCard.kt
+++ src/main/kotlin/entity/SchwimmenCard.kt
@@ -12,8 +12,8 @@ data class SchwimmenCard(val suit: CardSuit, val value: CardValue){
 
     /**
      * compares two [SchwimmenCard]s according to the [Enum.ordinal] value of their [CardSuit]
-     * (i.e., the order in which the suits are declared in the enum class)
      */
     operator fun compareTo(other: SchwimmenCard) = this.value.ordinal - other.value.ordinal
+
 
 }
