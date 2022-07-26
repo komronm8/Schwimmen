31706e03a0c9635ff02d8d1fa48017e7b08dad51
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-26 13:33:25 +0200

    Added Documentation and as well as an Init for the class itself

--- src/main/kotlin/entity/SchwimmenPlayer.kt
+++ src/main/kotlin/entity/SchwimmenPlayer.kt
@@ -1,5 +1,27 @@
 package entity
 
+/**
+ * Class for the player of the game itself. The [name] of the player, the [playerCards]
+ * and their total points that they have, are all contained in this class
+ *
+ * @param playerCards list of the cards that the player has
+ * @throws IllegalArgumentException if [name] is empty or [playerCards] is not equal to 3
+ */
 class SchwimmenPlayer(val playerCards: Array<SchwimmenCard>, val name: String){
+
     var points: Float = 0.0F
+
+    init{
+        if(name.isEmpty()){
+            throw IllegalArgumentException("Name must not be empty!")
+        }
+        if(playerCards.size != 3){
+            throw IllegalArgumentException("PLayer must have 3 playing cards!")
+        }
+    }
+
+    override fun toString(): String{
+        return "$name: ($points)"
+    }
+
 }
