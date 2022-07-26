37ddbec6eaba091e3abf428ceef79c78006fc077
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-26 12:52:47 +0200

    Added documentation to class SchwimmenCard

--- src/main/kotlin/entity/SchwimmenCard.kt
+++ src/main/kotlin/entity/SchwimmenCard.kt
@@ -1,9 +1,19 @@
 package entity
 
+/**
+ * Data class for the playing cards of the game "Schwimmen"
+ * With the parameters [CardSuit] and [CardValue]
+ */
 data class SchwimmenCard(val suit: CardSuit, val value: CardValue){
 
-    override fun toString() ="$suit$value"
+    override fun toString(): String{
+        return "$suit$value"
+    }
 
+    /**
+     * compares two [SchwimmenCard]s according to the [Enum.ordinal] value of their [CardSuit]
+     * (i.e., the order in which the suits are declared in the enum class)
+     */
     operator fun compareTo(other: SchwimmenCard) = this.value.ordinal - other.value.ordinal
 
 }
