d35b0a0f0b4133076091fa141b475e3994b05369
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-30 18:46:40 +0200

    Finished method swapOneCard in PlayerActionService.kt

--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -1,14 +1,41 @@
 package service
 
 import entity.SchwimmenCard
+import entity.SchwimmenGame
 import entity.SchwimmenPlayer
 
-class PlayerActionService(root: SchwimmenGameRootService): AbstractRefreshingService() {
+class PlayerActionService(private val root: SchwimmenGameRootService): AbstractRefreshingService() {
 
-    fun swapOneCard(tablecard: SchwimmenCard, playerCard: SchwimmenCard): Unit{}
+    fun swapOneCard(tableCard: SchwimmenCard, playerCard: SchwimmenCard): Unit{
+        val game = root.currentGame
+        checkNotNull(game)
+        val currentPlayer = game.players[game.currentPlayerIndex]
+        //checks if card exists in the tableCards or in the playerCards
+        if(tableCard !in game.tableCards || playerCard !in currentPlayer.playerCards){
+            throw IllegalStateException("The specified card does not exist!")
+        }
+        //Swaping of cards
+        for( i in game.tableCards.indices){
+            if( tableCard == game.tableCards[i] ){
+                game.tableCards[i] = playerCard
+            }
+        }
+        for( i in currentPlayer.playerCards.indices){
+            if( playerCard == currentPlayer.playerCards[i] ){
+                currentPlayer.playerCards[i] = tableCard
+            }
+        }
+        //configure passCount/playerPoints, refresh view and go to next player
+        game.passCount = 0
+        calculatePlayerPoints(currentPlayer)
+        onAllRefreshables { refreshAfterSwapCards() }
+        root.gameService.nextPlayer()
+    }
+    fun swapAllCards(): Unit{
+        val game = root.currentGame
+        checkNotNull(game)
 
-    fun swapOneCard(): Unit{}
-
+    }
     fun pass(): Unit{}
 
     fun knock(): Unit{}
