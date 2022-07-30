cd1a2d7b468fbdc6b6115d93b619661b783bb020
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-30 15:07:04 +0200

    Added method startNewGame

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -8,7 +8,17 @@ import tools.aqua.bgw.util.Stack
 class GameService(private val root: SchwimmenGameRootService): AbstractRefreshingService(){
 
     fun startNewGame(playerNames: Array<String>){
-        val cardStack = generateCards()
+        val stackOfCards = generateCards()
+        val tableCards = stackOfCards.popAll(3).toMutableList()
+        // checks if amount of playerNames are valid
+        if (playerNames.size !in 2..4) {
+            throw IllegalArgumentException("Number of players are not between 2 and 4!")
+        }
+        val players = mutableListOf<SchwimmenPlayer>()
+        for (i in playerNames.indices){
+            players.add(SchwimmenPlayer(stackOfCards.popAll(3).toMutableList(),playerNames[i]))
+        }
+        val game = SchwimmenGame(players, tableCards, stackOfCards)
 
     }
 
@@ -20,14 +30,14 @@ class GameService(private val root: SchwimmenGameRootService): AbstractRefreshin
 
     private fun generateCards(): Stack<SchwimmenCard>{
 
-        val cards = List(32) { index ->
+        val cardList = List(32) { index ->
             SchwimmenCard(
                 CardSuit.values()[index / 8],
                 CardValue.values()[(index % 8) + 5]
             )
         }.shuffled()
 
-        return Stack(cards)
+        return Stack(cardList)
     }
 
 }
