bd6b784c9f83d0fe4910efe383eec4c6df392045
Komron <komron.abdulloev@tu-dortmund.de>
2022-07-31 12:16:18 +0200

    Modified KDoc

--- src/main/kotlin/service/SchwimmenGameRootService.kt
+++ src/main/kotlin/service/SchwimmenGameRootService.kt
@@ -4,6 +4,8 @@ import entity.SchwimmenGame
 
 /**
  * Main class of the service layer for the "Schwimmen" Game. Connect the application layers.
+ * Provides access to all other service classes and holds the [currentGame] state for these
+ * services to access.
  */
 class SchwimmenGameRootService {
     val gameService = GameService(this)
