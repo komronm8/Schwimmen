06fc7779780eca994d053ff09768bf6819675393
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-02 17:05:45 +0200

    Changed name from HelloSCene to SchwimmenGameScene

--- src/main/kotlin/view/SchwimmenGameScene.kt
+++ src/main/kotlin/view/SchwimmenGameScene.kt
@@ -1,11 +1,14 @@
 package view
 
+import service.SchwimmenGameRootService
 import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.core.BoardGameScene
 import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
+import tools.aqua.bgw.visual.ImageVisual
 
-class HelloScene : BoardGameScene(500, 500) {
+class SchwimmenGameScene(private val rootService: SchwimmenGameRootService):
+    BoardGameScene(1920, 1080, ImageVisual("Background_green.png")) {
 
     private val helloLabel = Label(
         width = 500,
