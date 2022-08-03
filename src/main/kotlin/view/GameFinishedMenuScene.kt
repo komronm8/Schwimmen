442acee4326a3d8930c47c07f2d7f8b148a473ef
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-04 00:32:13 +0200

    Minor modifications done

--- src/main/kotlin/view/GameFinishedMenuScene.kt
+++ src/main/kotlin/view/GameFinishedMenuScene.kt
@@ -1,4 +1,8 @@
 package view
 
-class GameFinishedMenuScene {
+import service.SchwimmenGameRootService
+import tools.aqua.bgw.core.MenuScene
+
+class GameFinishedMenuScene(private val rootService: SchwimmenGameRootService):
+    MenuScene(400,1080), Refreshable {
 }
