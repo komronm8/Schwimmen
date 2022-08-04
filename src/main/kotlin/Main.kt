c1e815f905e5c09fee217c67ada2404196be25ae
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-04 13:52:21 +0200

    Added KDoc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import view.SchwimmenApplication
 
+/**
+ * main function that starts the [SchwimmenApplication]
+ */
 fun main() {
     SchwimmenApplication().show()
     println("Application ended. Goodbye")
