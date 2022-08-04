b7df8c4bcc230984ab0f4a5639e039f831d36dcd
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-04 13:44:31 +0200

    Added KDoc to the whole class

--- src/main/kotlin/view/Refreshable.kt
+++ src/main/kotlin/view/Refreshable.kt
@@ -1,9 +1,34 @@
 package view
 
+/**
+ * This interface provides a mechanism for the service layer classes to communicate
+ * (usually to the view classes) that certain changes have been made to the entity
+ * layer, so that the user interface can be updated accordingly.
+ */
 interface Refreshable {
+
+    /**
+     * perform refreshes that are necessary after a new game started
+     */
     fun refreshAfterStartNewGame(){}
+
+    /**
+     * perform refreshes that are necessary for when the player swaps his cards
+     */
     fun refreshAfterSwapCards(){}
+
+    /**
+     * perform refreshes that are necessary when the game ends
+     */
     fun refreshAfterGameEnd(){}
+
+    /**
+     * perform refreshes that are necessary when it is the next players turn
+     */
     fun refreshAfterNextPlayer(){}
+
+    /**
+     * perform refreshes that are necessary when every player passes
+     */
     fun refreshAfterAllPass(){}
 }
